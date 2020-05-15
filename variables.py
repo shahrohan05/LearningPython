ae409e70524a389b46da33cd8c90563e5b73801a
shahrohan05 <shahrohan07@live.com>
2020-05-15 17:00:44 +0530

    numbers strings and list data types

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
+# numbers and string data types examples
 mystring = "hello"
 myfloat = 10.0
 myint = 20
 
-# testing code
 if mystring == "hello":
     print("String: %s" % mystring)
 if isinstance(myfloat, float) and myfloat == 10.0:
@@ -16,7 +16,8 @@ print()
 print("Escaped String : \n \"Monty Python's Flying Circus\" \n\n")
 print('One more: \n "Monty Python\'s Flying Circus"\n\n')
 
-print(r"a row string  - \n \"Monty Python\'s Flying Circus") # raw string without treating \n as special.
+# a raw string, not treating \n as special.
+print(r"a row string  - \n \"Monty Python\'s Flying Circus")
 
 print(''' a
 multi line
@@ -28,7 +29,8 @@ multiline
 comment.
 """
 
-anotherString = "another" " string" # two or mote string literals next to each other are automatically concatenated.
+# two or mote string literals next to each other are automatically concatenated.
+anotherString = "another" " string"
 
 print("\n Another String : "+anotherString)
 
@@ -38,3 +40,14 @@ print(" 1 to 5 character of another string : "+anotherString[:5])
 
 print(" characters from 5th character of another string : "+anotherString[5:])
 
+# multiple assignment :
+a, b, c = 15, 15.5, "hello"
+print(a, b, c)
+
+# string repetition
+print('Circus Clown, '*3)
+
+# deleting variables: when you are sure you will never need the variable again in the program, or in situations where you can predict you may run out of memory.
+del a, b, c
+# this will throw NameError, since variables are deleted and no longer defined.
+print(a, b, c)
